8913b35e6072aaa7aa821580cdac541804e60f02
Dimacom <lqs5283829>
2021-03-06 13:31:48 +0300

    added ht06String Task1

---
+++ src/ht06String/Task1.java
@@ -0,0 +1,14 @@
+package ht06String;
+
+import java.util.Arrays;
+
+public class Task1 {
+
+    public static void main(String[] args) {
+        String string = "brother mother";
+        String[] words = string.split(" +");
+        Arrays.sort(words);
+        System.out.println(words[0]);
+    }
+}
+
