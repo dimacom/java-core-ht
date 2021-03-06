136d94701221f0da1a943560943b42793d21049e
Dimacom <lqs5283829>
2021-03-06 16:11:11 +0300

    added ht06String Task3

--- src/ht06String/Task1.java
+++ src/ht06String/Task1.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 public class Task1 {
 
     public static void main(String[] args) {
+
         String string = "brother mother";
         String[] words = string.split(" +");
         Arrays.sort(words);
---
+++ src/ht06String/Task3.java
@@ -0,0 +1,21 @@
+package ht06String;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Task3 {
+
+    public static void main(String[] args) {
+
+        final String regex = "\\b\\w+";
+        final String string = "banana - a. bax, c, d  e";
+        final Pattern pattern = Pattern.compile(regex, Pattern.MULTILINE);
+        final Matcher matcher = pattern.matcher(string);
+        int counter = 0;
+        while (matcher.find()) {
+            counter++;
+            if (counter == 1) System.out.println(matcher.group(0) + ":");
+            if (counter != 1) System.out.println("- " + matcher.group(0));
+        }
+    }
+}
