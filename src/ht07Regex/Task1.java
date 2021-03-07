9368ec0a8a68fbf1d28d6563fc9010566efab6b3
Dimacom <lqs5283829>
2021-03-07 23:24:06 +0300

    deleted mistake ht07Regex Task1

--- src/ht07Regex/Task1.java
+++ src/ht07Regex/Task1.java
@@ -7,14 +7,14 @@ public class Task1 {
 
     public static void main(String[] args) {
 
-        String regex = "\\s\\w+\\@\\w+\\.([com]{3}|[ru]{2}|[by]{2})";
+        String regex = "(\\w+@)(\\w+)(\\.\\w+)?(\\.\\w+)([com]|[by]|[ru])";
         String string = " mail@mail.ru, 2mail@mail.by, mail@mail..com, mail@@mail.com," +
-                        " mail@mail.mail.com, mail@mail@mail.com," +
+                        " mail@mail.mail.com, mail@mail@22mail.com," +
                         " @mail.com, mail.com, mail@mail, mail@.com  3mail@mail.com";
         Pattern pattern = Pattern.compile(regex, Pattern.MULTILINE);
         Matcher matcher = pattern.matcher(string);
         while (matcher.find()) {
-            System.out.println("Full match: " + matcher.group(0));
+            System.out.printf("Full match: [%s]\n", matcher.group(0));
         }
     }
 }
