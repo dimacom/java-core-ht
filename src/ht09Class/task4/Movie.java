6a586089266a8d2bebc1c368bed54594ba8e2351
Dimacom <lqs5283829>
2021-03-17 17:21:57 +0300

    added ht09 Task4

---
+++ src/ht09Class/task4/Movie.java
@@ -0,0 +1,18 @@
+package ht09Class.task4;
+
+public class Movie {
+    private String title;
+    private String desc;
+    private int year;
+
+    public Movie(String title, int year) {
+        this.title = title;
+        this.year = year;
+    }
+
+    public Movie(String title, String desc, int year) {
+        this.title = title;
+        this.desc = desc;
+        this.year = year;
+    }
+}
