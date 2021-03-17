eb54d16b4ddf870d0cb72ce2cd78758667ce30aa
Dimacom <lqs5283829>
2021-03-17 17:11:30 +0300

    added ht09 Task3

---
+++ src/ht09Class/task3/App.java
@@ -0,0 +1,8 @@
+package ht09Class.task3;
+
+public class App {
+    public static void main(String[] args) {
+        Account account = new Account(10,"123-456-789",200);
+        System.out.println(account.toString());
+    }
+}
