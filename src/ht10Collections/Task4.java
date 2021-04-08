f397abd5ab5b22957d17e394873278b572f3ef36
Dimacom <lqs5283829>
2021-04-08 13:24:19 +0300

    deleted mistake Task4

--- src/ht10Collections/Task4.java
+++ src/ht10Collections/Task4.java
@@ -2,6 +2,7 @@ package ht10Collections;
 
 import ht10Collections.person.Person;
 import ht10Collections.person.Season;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -9,9 +10,14 @@ public class Task4 {
     public static Map<Integer, Person> peopleBirthdayWithoutSeason(Season withoutSeason, Map<Integer, Person> people) {
         System.out.println("people who have birthday without " + withoutSeason.name());
         Map<Integer, Person> peopleBirthday = new HashMap<>();
+        String[] months = withoutSeason.season.split("\\D+");
         for (Person person : people.values()) {
-            if (!withoutSeason.season.contains(String.valueOf(person.getBirthday().getMonthValue()))) {
-                peopleBirthday.put(person.getIdPerson(), person);
+            int counter = 0;
+            for (String month : months) {
+                if (!month.equals(String.valueOf(person.getBirthday().getMonthValue()))) {
+                    ++counter;
+                    if (counter == 3) peopleBirthday.put(person.getIdPerson(), person);
+                }
             }
         }
         for (Person value : peopleBirthday.values()) {
